3c7189ad3c1a1b8b35ead4a7d70932438c7a6398
kjb244 <kevin.bacino@gmail.com>
2017-03-11 10:34:51 -0500

    service worker fixes

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
+console.log(__dirname + '/sw.js');
+
 
 app.use('/', index);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var jcrContent = require('../copy/testingcontent.json');
+
+
+router.get('/', function(req, res, next){
+	res.redirect('/testing');
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/testing', function(req, res, next) {
+  res.render('index', jcrContent);
 });
 
 module.exports = router;
