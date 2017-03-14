c150473867abb7bc92cd03373b585d439325c9c6
kjb244 <kevin.bacino@gmail.com>
2017-03-13 21:15:04 -0400

    removed service worker

--- app.js
+++ app.js
@@ -16,14 +16,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname));
-console.log(__dirname + '/sw.js');
 
 
 app.use('/', index);
