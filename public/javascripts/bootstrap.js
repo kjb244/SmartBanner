86120117375bd692106fe41850acb56ab3a91389
kjb244 <kevin.bacino@gmail.com>
2017-05-07 10:59:46 -0400

    more testing

---
+++ public/javascripts/bootstrap.js
@@ -0,0 +1,11 @@
+setTimeout(function(){
+	var htmlContent = $('html').html();
+	if (window.location.pathname === "/testing"){
+		$.post('htmlcontent', {'html': htmlContent}, function(data){
+
+			
+			window.location.href = '/testing2';
+		});
+	}
+
+	}, 2000);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var jcrContent = require('../copy/testingcontent.json');
-
+let html = '';
 
 router.get('/', function(req, res, next){
 	res.redirect('/testing');
@@ -10,6 +10,16 @@ router.get('/', function(req, res, next){
 /* GET home page. */
 router.get('/testing', function(req, res, next) {
   res.render('index', jcrContent);
+});
+
+router.post('/htmlcontent', function(req, res){
+	html = req.body.html;
+	res.render('pagehtml', {data1: html});
+});
+
+router.get('/testing2', function(req, res){
+	console.log('here');
+	res.render('pagehtml', {data1: html});
 });
 
 module.exports = router;
