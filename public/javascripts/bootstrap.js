75698090a9030e50cd4395b092b2a2bed607a91b
kjb244 <kevin.bacino@gmail.com>
2017-05-07 11:27:49 -0400

    more testign

--- public/javascripts/bootstrap.js
+++ public/javascripts/bootstrap.js
@@ -1,9 +1,9 @@
 window.onload = function() {
                 // Deep link to your app goes here
-                document.getElementById("l").src = "my_app://";
+                window.location = "bofa://";
 
                 setTimeout(function() {
                     // Link to the App Store should go here -- only fires if deep link fails                
-                    window.location = "https://itunes.apple.com/us/app/bank-of-america-mobile-banking/id284847138?mt=8";
+                    //window.location = "https://itunes.apple.com/us/app/bank-of-america-mobile-banking/id284847138?mt=8";
                 }, 500);
             };
