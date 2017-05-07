8b520ac0c706225631e2096157991c0e09c2dcec
kjb244 <kevin.bacino@gmail.com>
2017-05-07 19:17:10 -0400

    testing

--- public/javascripts/bootstrap.js
+++ public/javascripts/bootstrap.js
@@ -1,12 +1,13 @@
 
 
+
 $(document).ready(function(){
 
 	$('.btn').on('click', function(){
-		window.location = 'boa://';
-		alert('installed');
+		window.location = 'bofa://';
+
 		setTimeout(function(){
-			alert('not installed');
+	
 			window.location = 'https://itunes.apple.com/us/app/bank-of-america-mobile-banking/id284847138?mt=8';
 
 		},200);
